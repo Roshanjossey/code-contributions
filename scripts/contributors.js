ad74658b7d2a8dbb1a8690d952947166c588bea9
ricky-aufvaa <aufvaa@gmail.com>
2025-03-08 00:15:13 +0530

    add ricky-aufvaa

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -2,3 +2,8 @@ const contributorFiles = [
   "roshanjossey.html",
   "gokultp.html",
 ];
+const contributorFiles = [
+  "ricky-aufvaa.html", // add your file name here
+  "roshanjossey.html",
+  "gokultp.html",
+];
