48f86240353e0f9c1e555320965141fd4b4f0156
Roshan Jossy <roshanjossey@gmail.com>
2025-03-08 01:32:46 +0100

    Update contributors.js

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -2,8 +2,3 @@ const contributorFiles = [
   "roshanjossey.html",
   "gokultp.html",
 ];
-const contributorFiles = [
-  "ricky-aufvaa.html", // add your file name here
-  "roshanjossey.html",
-  "gokultp.html",
-];
