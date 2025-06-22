893ed67043a5c71efa5cceed4334bf2da7c97bad
xarical <173563648+xarical@users.noreply.github.com>
2025-06-21 17:59:05 -0700

    Fix workflow to ensure the base branch's check_pr.py script is run, to handle more complex commit history, and to write newlines correctly in the comment

--- scripts/check_pr.py
+++ scripts/check_pr.py
@@ -53,8 +53,12 @@ def file_is_toxic(file_path: str) -> bool:
 
 
 if __name__ == "__main__":
+    base = sys.argv[1]
+    head = sys.argv[2]
     toxic = False
-    for file_path in os.popen("git diff --name-only HEAD^ HEAD").read().split(): # For each file in the diff,
+    for file_path in os.popen(
+        f"git diff --name-only {base}...{head}"
+    ).read().split(): # For each file in the diff,
         if os.path.exists(file_path) and file_is_toxic(file_path): # Check it if it exists and is toxic
             print(f"🚩 Flagged {file_path}")
             toxic = True
