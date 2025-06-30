68e7e1002f6072d7350a58873fa5823e554a5e88
Afzal Ansari <ansarahemad1220@gmail.com>
2025-07-01 00:45:33 +0530

    added color

--- scripts/load-cards.js
+++ scripts/load-cards.js
@@ -1,9 +1,10 @@
 function createCard(contributor) {
     const iframe = document.createElement('iframe');
-    iframe.className = 'card col col-6-md col-3-lg bd-grey';
+    iframe.className = 'card col col-6-md col-3-lg bd-green';
     iframe.src = `contributors/${contributor}`;
     iframe.title = contributor
     document.getElementById('contributor-cards').appendChild(iframe);
 }
 
 contributorFiles.forEach(contributor => createCard(contributor));
+
