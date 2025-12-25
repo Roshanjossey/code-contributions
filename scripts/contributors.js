ddbae76ecdb338f208549bd05231b5e17ed5e411
Emmanuel <kachinnagbo3328@gmail.com>
2025-12-25 02:25:47 +0100

    add Onyedika1234

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -1,4 +1,2 @@
-const contributorFiles = [
-  "roshanjossey.html",
-  "gokultp.html",
-];
+const contributorFiles = ["roshanjossey.html", "gokultp.html"];
+const contributor_files = ["Onyedika1234.html", "Onyedika1234.html"];
