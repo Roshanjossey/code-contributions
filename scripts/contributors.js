9e8b2f85a824d34ff4364a9a89b0e228c5f94e05
hamsehmahamud <hamsehmahamud@gmail.com>
2026-08-06 21:33:03 +0300

    docs:scripts/contributors.js and contributors/hamsehmahamud.html

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -2,3 +2,8 @@ const contributorFiles = [
   "roshanjossey.html",
   "gokultp.html",
 ];
+const contributorFiles = [
+  "hamsehmahamud.html", // add your file name here
+  "roshanjossey.html",
+  "gokultp.html",
+];
